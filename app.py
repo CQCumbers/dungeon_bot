8bd60673f1cceb20fc9c250d414a8f1162929a40
Alexander <alexbrianzhang@gmail.com>
2020-01-10 16:17:53 -0500

    Add more commands, rent machine with enough vram

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import aiohttp, aioredis
 from discord.ext import commands
 
 api_url = 'https://vast.ai/api/v0'
-image = 'cqcumbers/dungeon_worker:0.1.2'
-timeout, max_msgs = 900.0, 20
+image = 'cqcumbers/dungeon_worker:0.1.3'
+timeout, max_msgs = 600.0, 4
 bot = commands.Bot(command_prefix='!')
 
 
@@ -22,84 +22,85 @@ async def send(session, url, data, params={}):
 async def create_inst(session):
     # Query available machines
     params = {'q': json.dumps({
-        'verified': {'eq': True},
-        'external': {'eq': False},
-        'rentable': {'eq': True},
-        'cuda_max_good': {'gte': 10.0},
-        'inet_up': {'gte': 100.0},
-        'reliability2': {'gte': 0.97},
-        'storage': 15.0, 'type': 'on-demand',
-        'order': [['dph_total', 'asc']],
+        'verified': {'eq': True}, 'external': {'eq': False},
+        'rentable': {'eq': True}, 'disk_space': {'gte': 15.0},
+        'gpu_ram': {'gte': 10.0 * 1024}, 'inet_up': {'gte': 100.0},
+        'reliability2': {'gte': 0.90}, 'allocated_storage': 15.0,
+        'dlperf': {'gte': 9.8}, 'type': 'ask', 'order': [['dphtotal', 'asc']],
     })}
     data = await fetch(session, f'{api_url}/bundles', params)
     bot.inst_id = data['offers'][0]['id']
 
     # Create instance using cheapest machine
-    print('Creating new GPU instance')
     onstart = (
         f'export REDIS_URL={os.getenv("REDIS_EXTERN_URL")}\n'
         f'export DISCORD_TOKEN={os.getenv("DISCORD_TOKEN")}\n'
-        f'cd / && python process_queue.py\n')
+        f'cd / && script -c "python process_queue.py" -a /root/process.log\n')
     config = {
         'client_id': 'me', 'image': image,
         'runtype': 'ssh', 'disk': 15.0,
         'label': 'dungeon_worker', 'onstart': onstart,
     }
     await send(session, f'{api_url}/asks/{bot.inst_id}/', config)
+    print(f'Created instance {bot.inst_id}')
 
 
 async def restart_inst(session, insts):
-    print('Restarting GPU instance')
+    # send start instance request to first in list
     bot.inst_id, data = insts[0]['id'], {'state': 'running'}
     await send(session, f'{api_url}/instances/{bot.inst_id}/', data)
+    print(f'Restarted instance {bot.inst_id}')
 
 
-async def stop_inst(session):
-    while asyncio.get_event_loop().time() < bot.stop_time:
-        await asyncio.sleep(1)
-    print('Stopping GPU instance')
-    data = {'state': 'stopped'}
-    await send(session, f'{api_url}/instances/{bot.inst_id}/', data)
-    bot.inst_id = None
+async def stop_inst():
+    # wait until no activity and past timeout
+    while bot.loop.time() < bot.stop_time:
+        await asyncio.sleep(1.0)
+    # send stop instance request
+    async with aiohttp.ClientSession() as session:
+        data = {'state': 'stopped'}
+        await send(session, f'{api_url}/instances/{bot.inst_id}/', data)
+        print(f'Stopped instance {bot.inst_id}'); bot.inst_id = None
 
 
-async def init_inst():
-    bot.inst_id = 0xdeadbeef
+async def init_inst(ctx):
+    bot.inst_id = 123456789
+    await ctx.send('Initializing instance, please wait')
     # reboot instance, or create one if none exists
     async with aiohttp.ClientSession() as session:
         data = await fetch(session, f'{api_url}/instances', {'owner': 'me'})
         insts = data.get('instances')
         await restart_inst(session, insts) if insts else await create_inst(session) 
-        bot.stop_time = asyncio.get_event_loop().time() + timeout
-        bot.stop_inst = asyncio.create_task(stop_inst(session))
-    print(f'Using instance {bot.inst_id}')
+        bot.stop_inst = bot.loop.create_task(stop_inst())
 
 
 async def init_queue():
     bot.queue = await aioredis.create_redis_pool(os.getenv('REDIS_URL'))
 
 
-async def send_msg(message):
+@bot.command(name='next', help='Continues AI Dungeon game')
+async def game_next(ctx, *, text):
+    message = {'channel': ctx.channel.id, 'text': text}
+    bot.stop_time = bot.loop.time() + timeout
+
     await init_queue() if not bot.queue else None
     if await bot.queue.llen('msgs') < max_msgs:
         await bot.queue.lpush('msgs', json.dumps(message))
-    bot.stop_time = asyncio.get_event_loop().time() + timeout
-    await init_inst() if not bot.inst_id else None
+    await init_inst(ctx) if not bot.inst_id else None
 
 
-@bot.command(name='next', help='Continues AI Dungeon game')
-async def game_next(ctx, *, text):
-    await send_msg({'channel': ctx.channel.id, 'text': text})
+@bot.command(name='restart', help='Starts the game from beginning')
+async def game_restart(ctx):
+    await init_queue() if not bot.queue else None
+    await bot.queue.delete(ctx.channel.id)
+    await ctx.send('Restarted game from beginning')
 
 
-#@bot.command(name='restart', help='Starts the game from beginning')
-#async def game_restart(ctx):
-#    send_msg(json.dumps({'channel': ctx.channel.id, 'action': 'restart'}))
-
-
-#@bot.command(name='revert', help='Undoes the last action')
-#async def game_revert(ctx):
-#    send_msg(json.dumps({'channel': ctx.channel.id, 'action': 'undo'}))
+@bot.command(name='revert', help='Undoes the last action')
+async def game_revert(ctx):
+    await init_queue() if not bot.queue else None
+    await bot.queue.rpop(ctx.channel.id)
+    await ctx.send('Undid last action')
 
 
 if __name__ == '__main__':
--- worker/process_queue.py
+++ worker/process_queue.py
@@ -1,4 +1,4 @@
-import os, json, aioredis, discord
+import os, json, asyncio, aioredis, discord
 from generator.gpt2.gpt2_generator import *
 
 max_history = 20
@@ -9,21 +9,24 @@ generator = GPT2Generator()
 async def on_ready():
     # connect & clear redis queue
     queue = await aioredis.create_redis(os.getenv('REDIS_URL'))
+    loop = asyncio.get_event_loop()
     while await queue.llen('pending'):
-        await queue.brpoplpush('pending', 'msgs')
+        await queue.rpoplpush('pending', 'msgs')
 
     while True:
         # poll queue for messages, block here if empty
         message = await queue.brpoplpush('msgs', 'pending')
         print(f'Processing message: {message}')
         args = json.loads(message)
-        channel, text = args['channel'], '> ' + args['text']
+        channel, text = args['channel'], f'\n> {args["text"]}\n'
 
         # generate response, update conversation history
-        history = await queue.lrange(channel, 0, max_history - 1)
-        response = generator.generate('\n'.join(history + [text]))
-        await queue.rpush(channel, text, response)
-        await queue.ltrim(channel, -max_history, -1)
+        async with client.get_channel(channel).typing():
+            history = await queue.lrange(channel, 0, max_history - 1)
+            response = await loop.run_in_executor(
+                None, generator.generate, ''.join(history + [text]))
+            await queue.rpush(channel, text, response)
+            await queue.ltrim(channel, -max_history, -1)
 
         # send response, delete message from queue
         await client.get_channel(channel).send(response)
